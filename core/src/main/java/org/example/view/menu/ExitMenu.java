2769f81733279f7eb627e3ba14d0f891443d6742
rassa <rassa.mohammadi@gmail.com>
2025-06-17 23:30:21 +0330

    commit delete

--- core/src/main/java/org/example/view/menu/ExitMenu.java
+++
@@ -1,9 +0,0 @@
-package org.example.view.menu;
-
-import org.example.view.AppMenu;
-
-import java.util.Scanner;
-
-public class ExitMenu extends AppMenu {
-    public void executeCommands(Scanner scanner) {}
-}
